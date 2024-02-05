1763030b60b38cb1891762a7993a9cb60344451c
majohurt28 <113456623+majohurt28@users.noreply.github.com>
2024-02-05 13:16:57 -0500

    CSS ADVANCE

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,6 +18,7 @@ export function Header(props){
                     <h1> CANVAS</h1>
                 </header>
                 <img className="Banner" src= "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhi3hXTDgeKmxWAosJN6xtbvrIjXF768fnoQ4oOCjTa8Gx4JGzmo40DNRVms90Aw0JCLdcwuqAkxPbOpwRMqPNbRJgd6B1GkTsvEpWmZjMeu5FdyYQW_6tlPXGHgvDQquJs6nePiWV5ATTm/w603-h334/Webtoon%252C+American+Road+Trip+Banner+Art_Logo.png" />
+                <img className="Background" src= "https://webtoons-static.pstatic.net/image/pc/home_bg004.jpg" />
 
 
             </div>
