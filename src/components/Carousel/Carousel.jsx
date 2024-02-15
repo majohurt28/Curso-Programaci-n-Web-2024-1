8791dc4225d8866b9f53d3e53d528a8771bc8886
majohurt28 <113456623+majohurt28@users.noreply.github.com>
2024-02-14 19:33:08 -0500

    Advanced Responsive design

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -3,8 +3,8 @@ import React from "react"
 
 export function Carousel(props){
     return(
-        <div style={{height: "500px", width: "1503px", height: "500px", backgroundImage: "url('https://webtoons-static.pstatic.net/image/pc/home_bg004.jpg')"}} className='carouselContainer' >
+        <div style={{}} className='carouselContainer' >
                 <img className="Banner" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhi3hXTDgeKmxWAosJN6xtbvrIjXF768fnoQ4oOCjTa8Gx4JGzmo40DNRVms90Aw0JCLdcwuqAkxPbOpwRMqPNbRJgd6B1GkTsvEpWmZjMeu5FdyYQW_6tlPXGHgvDQquJs6nePiWV5ATTm/w603-h334/Webtoon%252C+American+Road+Trip+Banner+Art_Logo.png" />
-                {/* <img style={{ paddingTop: "5px"}} className="Background" src="https://webtoons-static.pstatic.net/image/pc/home_bg004.jpg" /> */}
+                <img  className="Background-mobile" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F351912460975274%2F&psig=AOvVaw1qAMJ_PRS9f2Ifv7DF3PgQ&ust=1708042003303000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJiX7riGrIQDFQAAAAAdAAAAABBi" />
         </div>
     )}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,18 +7,9 @@ export function Header(props) {
     const { content, color } = props
     return (
         <header style={{
-            color: color,
-            display:'flex',
-            flexDirection:'row',
-            justifyContent:'space-evenly',
-            backgroundColor: "#ffffff",
-            fontFamily:"hind,simhei,verdana,Helvetica,sans-serif",
-            width: "100%",
-            height: "99px",
-
-
         }}>
-        <div style= {{display: "flex", flexDirection: "row", marginLeft: "-100px", width: "630px", fontSize: "20px", fontWeight: "700"}}>
+
+        <div className="options" style= {{}}>
             <img  style={{ width: "100px", height: "97"}} className="Logo" src="https://upload.wikimedia.org/wikipedia/commons/0/09/Naver_Line_Webtoon_logo.png" />
             <nav >
                 <ul style= {{display: "flex", flexDirection: "row", fontSize: "20px", marginLeft: "10px", paddingLeft: "20px", marginTop: "40px",  }}>
@@ -30,19 +21,13 @@ export function Header(props) {
             </nav>
         </div>
 
-        <div className="buttonContainer" style= {{display: "flex", flexDirection: "row", marginTop: "16px"}}>
+        <div className="buttonContainer">
                 <h1>WEBTOON SHOP</h1>
                 <h2><img className="bookIcon" src={book}></img>Creators 101</h2>
-                <button style={{background: "#2f2f2f",
-                                borderRadius: "17px",
-                                color: "#fff",
-                                fontSize: "14px",
-                                fontWeight: "500",
-                                height: "33px", width: "104px",
-                                marginLeft: "13px"}}>
+                <button className="Publish-button" style={{}}>
                 Publish</button>
 
-                <button style={{backgroundColor: "#ffffff",
+                <button className="User-button" style={{backgroundColor: "#ffffff",
                                 borderRadius: "17px",
                                 color: "#7f7f7f",
                                 fontSize: "13px",
@@ -52,6 +37,7 @@ export function Header(props) {
                                 height: "33px", width: "104px",
                                 marginLeft: "7px"}}>
                 majo osorio</button>
+                <button className="len-button"><img  style={{}} className="len"src={len}></img></button>
             </div>
             {/* <div className="len-container">
                 <img style={{width: "40px", height: "40px"}} src={len}></img>
@@ -69,27 +55,6 @@ export function Header(props) {
             {/* <div className="bar-container" style={{
                 width: "1518pxs", position: "relative", left: "0px", top: "-26px"
 
-            }}>
-
-                <div style={{ width: "300px", marginLeft: "30px" }}>
-                    <img style={{ marginLeft: "14px", top: "10px", width: "90px", height: "90px" }} className="Logo" src="https://upload.wikimedia.org/wikipedia/commons/0/09/Naver_Line_Webtoon_logo.png" />
-
-                    <h1> ORIGINALS</h1>
-                    <h1> GENRES</h1>
-                    <h1> POPULAR</h1>
-                    <h1> CANVAS</h1>
-
-                    <span style={{ color: "#00dc64", fontweight: "900", fontSize: "20px", width: "156px" }}>WEBTOON SHOP</span>
-                    <h1 style={{ color: "#000", width: "90px" }}> Creators 101</h1>
-                    <button style={{ borderRadius: "20px" }}>Publish </button>
-                </div>
-
-
-
-
-                <img className="mobilBack" src="https://static.wikia.nocookie.net/webtoon/images/0/04/C%C3%B3mo_domar_una_fiera_encantadora.jpg/revision/latest/smart/width/250/height/250?cb=20210926185028&path-prefix=es" />
-                <img className="Banner" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhi3hXTDgeKmxWAosJN6xtbvrIjXF768fnoQ4oOCjTa8Gx4JGzmo40DNRVms90Aw0JCLdcwuqAkxPbOpwRMqPNbRJgd6B1GkTsvEpWmZjMeu5FdyYQW_6tlPXGHgvDQquJs6nePiWV5ATTm/w603-h334/Webtoon%252C+American+Road+Trip+Banner+Art_Logo.png" />
-                <img className="Background" src="https://webtoons-static.pstatic.net/image/pc/home_bg004.jpg" />
 
 
 
@@ -98,10 +63,4 @@ export function Header(props) {
         </header>
     )
 }
-
-/*     return(
-        <header style= {{backgroundColor: Color}}>Buenas noches</header>
-    )
-}
- */
 
