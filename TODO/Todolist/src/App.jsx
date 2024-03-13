0540eafbbf58eb5fd7c7fa1a4cbd415cfa6c60ce
majohurt28 <113456623+majohurt28@users.noreply.github.com>
2024-03-13 18:01:33 -0500

    app state

--- TODO/Todolist/src/App.jsx
+++ TODO/Todolist/src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from 'react'
 import { Header, Body, Footer, TaskList, Form, ToDo, Filter } from './components/index'
 
 /**Trabajo presentado por María José Hurtado y Nicolás Rodriguez**/
 export default function App() {
+    const [tasks, setTasks] = useState([
+        {
+        id: crypto.randomUUID(),
+        tittle: '...',
+        completed: 'false',
+
+        },
+
+    ])
     return (
         <div className='app-container'>
             <Header/>
             <Form></Form>
             <Filter></Filter>
-            <TaskList>
-                <ToDo/>
-                <ToDo/>
-                <ToDo/>
-                <ToDo/>
-                <ToDo/>
+            <TaskList tasks={tasks}>
             </TaskList>
         </div>
     )
--- TODO/Todolist/src/components/Filter/Filter.jsx
+++ TODO/Todolist/src/components/Filter/Filter.jsx
@@ -5,12 +5,17 @@ export function Filter() {
     return (
         <div className='filter-cont' >
             <FilterContainer>
-                <CountTask></CountTask>
                 <ButtonContainer>
-                    <ButtonFilter action={()=> {}} active="All" filter="All"/>
-                    <ButtonFilter action={()=> {}} active="All" filter="Completed"/>
-                    <ButtonFilter action={()=> {}} active="All" filter="Pending"/>
+                        <ButtonFilter action={()=> {}} active="All" filter="All"/>
+                        <ButtonFilter action={()=> {}} active="All" filter="Completed"/>
+                        <ButtonFilter action={()=> {}} active="All" filter="Pending"/>
                 </ButtonContainer>
+            <div className='count-clear'>
+                <CountTask></CountTask>
+                <button className='delete-tasks'>Clear all</button>
+            </div>
+
+
             </FilterContainer>
         </div>
     )
